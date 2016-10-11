1be77fbe2d44716c4dcceb658103118eba6e6946
Austin Morrell <austinlm19@gmail.com>
2016-10-11 16:03:36 -0500

    first commit

---
+++ Assets/Scripts/Seak.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Seak : MonoBehaviour {
+
+    [SerializeField]
+    GameObject Box;
+
+	void Start ()
+    {
+	
+	}
+	
+	void Update ()
+    {
+        Lerp(Box.transform.position.x * Time.deltaTime,
+            Box.transform.position.y * Time.deltaTime,
+            Box.transform.position.z * Time.deltaTime);
+	}
+
+    void Lerp(float a, float b, float c)
+    {
+        gameObject.transform.position = new Vector3(
+            transform.position.x + a, 
+            transform.position.y + b, 
+            transform.position.z + c);
+    }
+}
