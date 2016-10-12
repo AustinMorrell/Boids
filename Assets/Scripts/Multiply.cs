b3185037966d24306e9fd202f5d6bd55c54583b2
Austin Morrell <austinlm19@gmail.com>
2016-10-12 12:52:34 -0500

    The birth of the universe

---
+++ Assets/Scripts/Multiply.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Multiply : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject SpherePre;
+
+    [SerializeField]
+    private float numbSpheres;
+
+    private bool rev;
+    // Use this for initialization
+    void Start ()
+    {
+        for (int i = 0; i < numbSpheres; i++)
+        {
+            Vector3 position = new Vector3(Random.Range(-20.0f, 20.0f), Random.Range(-20.0f, 20.0f), Random.Range(-20.0f, 20.0f));
+            Quaternion spawnRotation = Quaternion.identity;
+            Instantiate(SpherePre, position, spawnRotation);
+        }
+    }
+}
