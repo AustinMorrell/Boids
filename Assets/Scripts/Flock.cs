80eedee2c01eb325b31843c8118ca67e2058b524
Austin Morrell <austinlm19@gmail.com>
2016-12-19 14:26:41 -0600

    Added Sliders, They are broken

--- Assets/Scripts/Flock.cs
+++ Assets/Scripts/Flock.cs
@@ -43,7 +43,7 @@ public class Flock : MonoBehaviour {
     // --------------------------------------------
     void Update()
     {
-        Vector3 v1, v2, v3, v4, v5, v6, v7;
+        Vector3 v1, v2, v3, v4, v5, v6;
         foreach (Seak b in DemBoids)
         {
             if (!perching)
---
+++ Assets/Scripts/GeneralManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GeneralManager : MonoBehaviour {
+
+    private float coh = 5.0f;
+    private float dis = 10.0f;
+    private float all = 25.0f;
+
+        // Update is called once per frame
+    void Update()
+    {
+        foreach (Flock a in FindObjectsOfType<Flock>())
+        {
+            a.cohesion = coh;
+            a.dispersion = dis;
+            a.alliment = all;
+        }
+    }
+
+    public void setCoh(float d)
+    {
+        coh = d;
+    }
+
+    public void setDis(float d)
+    {
+        dis = d;
+    }
+
+    public void setAll(float d)
+    {
+        all = d;
+    }
+}
